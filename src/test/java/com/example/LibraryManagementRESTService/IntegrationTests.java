4628ce01dd633d24ac9c159d0907f87f630f8058
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-07-01 03:09:14 +0100

    Create booksEntityWorksThroughAllLayers Integration Test

---
+++ src/test/java/com/example/LibraryManagementRESTService/IntegrationTests.java
@@ -0,0 +1,53 @@
+package com.example.LibraryManagementRESTService;
+
+import com.example.LibraryManagementRESTService.model.Book;
+import com.example.LibraryManagementRESTService.repository.BookRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringJUnit4ClassRunner.class)
+public class IntegrationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    @DisplayName("Books Entity works through all layers")
+    public void booksEntityWorksThroughAllLayers() throws Exception {
+        Book book = new Book(UUID.randomUUID().toString(), "testName", "testAuthor", "testCategory");
+
+
+        mockMvc.perform(post("/addbook")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(book)))
+                .andExpect(status().isOk());
+
+        Book testBook = bookRepository.findByName("testName");
+        assertThat(testBook.getName()).isEqualTo(book.getName());
+    }
+
+}
