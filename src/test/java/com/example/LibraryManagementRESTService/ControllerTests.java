f2ffab6f83a502acabe8d65b33776c32e78f729c
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-07-07 21:56:02 +0100

    Create Integration Tests in ControllerTests class to test Delete mapping and Put mapping when renting a book

--- src/test/java/com/example/LibraryManagementRESTService/ControllerTests.java
+++ src/test/java/com/example/LibraryManagementRESTService/ControllerTests.java
@@ -3,17 +3,22 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.LibraryManagementRESTService.model.Book;
+import com.example.LibraryManagementRESTService.repository.BookRepository;
 import com.example.LibraryManagementRESTService.service.BookService;
 import com.example.LibraryManagementRESTService.web.Controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 //import org.junit.Assert;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
@@ -21,9 +26,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -33,9 +40,14 @@ import org.assertj.core.api.Assert;
 import java.util.Optional;
 
 
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@AutoConfigureMockMvc
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = LibraryManagementRestServiceApplication.class)
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
 public class ControllerTests {
 
     @Autowired
@@ -44,16 +56,38 @@ public class ControllerTests {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @Autowired
     private BookService bookService;
 
-    @Autowired
-    private Controller controller;
+    @MockBean
+    private BookRepository mockRepository;
+
+    private int id = 1;
+
+    public Book createBook(int quantity) {
+        Book book = new Book("name", "author", "category", quantity);
+        book.setId(id);;
+        id ++;
+        return book;
+    }
+
+    public Book createBook() {
+        Book book = new Book("name", "author", "category", 5);
+        book.setId(id);;
+        id ++;
+        return book;
+    }
 
 
     @BeforeEach
     public void setUp(WebApplicationContext webApplicationContext) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Before
+    public void init() {
+        Book book = createBook();
+        when(mockRepository.findById(1)).thenReturn(book);
     }
 
     @Test
@@ -71,7 +105,7 @@ public class ControllerTests {
 
     @Test
     public void testSaveToDatabase() throws Exception {
-        Book book = new Book("name", "author", "cat", 5);
+        Book book = createBook();
 
         String bookJson = objectMapper.writeValueAsString(book);
 
@@ -86,5 +120,28 @@ public class ControllerTests {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    public void deleteBookWithIdOne_thenReturn200() throws Exception {
+        mockRepository.save(createBook());
+        mockMvc.perform(delete("/delete/{id}", 1))
+                .andExpect(status().isOk());
+    }
+
+
+    @Test
+    public void whenRentBook_thenStatus200() throws Exception {
+        Book book = createBook();
+        mockRepository.save(book);
+        System.out.println(book.getId());
+        mockMvc.perform(put("/rent/1"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/1")
+                        .content(objectMapper.writeValueAsString(book))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.quantity", Matchers.equalTo(book.getQuantity() - 1)));
+    }
 
 }
--- src/test/java/com/example/LibraryManagementRESTService/ServiceTests.java
+++ src/test/java/com/example/LibraryManagementRESTService/ServiceTests.java
@@ -5,31 +5,31 @@ import com.example.LibraryManagementRESTService.model.Library;
 import com.example.LibraryManagementRESTService.repository.BookRepository;
 import com.example.LibraryManagementRESTService.service.BookService;
 import com.example.LibraryManagementRESTService.web.Controller;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+// This class uses Mockito to test BookService
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class MockitoTests {
+public class ServiceTests {
 
     @Autowired
     private MockMvc mockMvc;
@@ -55,7 +55,7 @@ public class MockitoTests {
     @BeforeEach
     public void setUp(WebApplicationContext webApplicationContext) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-        bookService = new BookService(bookRepository);
+        bookService =  mock(BookService.class);
     }
 
     @Test
@@ -67,7 +67,10 @@ public class MockitoTests {
     @Test
     public void testGetAllAvailable() {
         when(bookService.getAllAvailable()).thenReturn(new ArrayList<Book>());
-        assertEquals(new ArrayList<Book>(), bookService.getAllAvailable());
+         List<Book> bookList = new ArrayList<Book>();
+         assertEquals(bookList, bookService.getAllAvailable());
     }
+
+    
 
 }
